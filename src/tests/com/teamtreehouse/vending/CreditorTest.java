b9d1c9481a3e1b864814c5024cec267507425c3a
justinm-nyc <justin.martinelli@oracle.com>
2019-11-06 16:23:32 -0500

    Added passing test

--- src/tests/com/teamtreehouse/vending/CreditorTest.java
+++ src/tests/com/teamtreehouse/vending/CreditorTest.java
@@ -7,11 +7,16 @@ import static org.junit.Assert.*;
 public class CreditorTest {
 
     @Test
-    public void refund() {
+    public void addingFundsIncrementsAvailableFunds() {
+        Creditor creditor = new Creditor();
+
+        creditor.addFunds(25);
+        creditor.addFunds(25);
+
+        assertEquals(50, creditor.getAvailableFunds());
     }
 
     @Test
-    public void addingFundsIncrementsAvailableFunds() {
-
+    public void refundingReturnsAllAvailableFunds() {
     }
 }
