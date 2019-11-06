7ecffa06f470f19a7cf39f5db330c4e9f3b39dc8
justinm-nyc <justin.martinelli@oracle.com>
2019-11-06 16:14:06 -0500

    Added first test

---
+++ src/tests/com/teamtreehouse/vending/CreditorTest.java
@@ -0,0 +1,17 @@
+package com.teamtreehouse.vending;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CreditorTest {
+
+    @Test
+    public void refund() {
+    }
+
+    @Test
+    public void addingFundsIncrementsAvailableFunds() {
+
+    }
+}
